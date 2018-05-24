7021752d2154b8b8184429138e9c03bbc4473ccf
porter-hu <571137089@qq.com>
2018-05-24 20:05:56 +0800

    声明类

--- app.ts
+++ app.ts
@@ -1,54 +1,67 @@
 import api = require("./api");
 import cheerio = require("cheerio");
 import fs = require("fs");
+
+export class Channel {
+  ChannelName: string;
+  Contents: Content[];
+}
+export class Content {
+  Url: string;
+  Title: string;
+  CreateTime: Date;
+}
+
 const go = async () => {
   const res = await api.remote_get("http://www.gl-ns.com/");
   //加载HTML
   const $ = cheerio.load(res.text);
   let Contents: object[] = [];
-  let news: object[] = [];
+  let news: Channel[] = [];
   let urls: string[] = [];
   let titles: string[] = [];
   let data: string[] = [];
 
-  $(".new").eq(0).each((index, element) => {
-    news.push({
-      ChannelName: $(element)
-        .find(".l")
-        .first()
-        .text()
-        .trim(),
-      Contents: Contents
-    });
+  $(".new")
+    .eq(0)
+    .each((index, element) => {
+      news.push({
+        ChannelName: $(element)
+          .find(".l")
+          .first()
+          .text()
+          .trim(),
+        Contents: Contents
+      });
 
-    // console.log(news[index].Contents);
-    $(element)
-      .find("li")
-      .each((ind, elet) => {
-        // for (let i = 0; i < news.length; i++) {
-        //   const item = news[i].Contents;
-        // }
-        // console.log(ind);
-        Contents.push({
-          Url:
-            "http://www.gl-ns.com" +
-            $(elet)
+      // console.log(news[index].Contents);
+      $(element)
+        .find("li")
+        .each((ind, elet) => {
+          // for (let i = 0; i < news.length; i++) {
+          //   const item = news[i].Contents;
+          // }
+          // console.log(ind);
+          Contents.push({
+            Url:
+              "http://www.gl-ns.com" +
+              $(elet)
+                .find("a")
+                .first()
+                .attr("href"),
+            Title: $(elet)
               .find("a")
               .first()
-              .attr("href"),
-          Title: $(elet)
-            .find("a")
-            .first()
-            .text()
-            .trim(),
-          CreateTime: $(elet)
-            .find(".time")
-            .first()
-            .text()
-            .trim()
+              .text()
+              .trim(),
+            CreateTime: $(elet)
+              .find(".time")
+              .first()
+              .text()
+              .trim()
+          });
         });
-      });
-  });
+    });
   // console.log(news);
   fs.writeFile("./text.json", JSON.stringify(news), (err: object) => {
     if (err) {
