a09360b8dc34f0f6bed6af653f021628445f2d9d
porter-hu <571137089@qq.com>
2018-05-25 19:34:01 +0800

    2018.02.25

--- app.ts
+++ app.ts
@@ -9,84 +9,64 @@ export class Channel {
 export class Content {
   Url: string;
   Title: string;
-  CreateTime: Date;
+  CreateTime: string;
 }
 
 const go = async () => {
   const res = await api.remote_get("http://www.gl-ns.com/");
   //加载HTML
   const $ = cheerio.load(res.text);
-  let Contents: object[] = [];
   let news: Channel[] = [];
-  let urls: string[] = [];
-  let titles: string[] = [];
-  let data: string[] = [];
 
-  $(".new")
-    .eq(0)
-    .each((index, element) => {
-      news.push({
-        ChannelName: $(element)
-          .find(".l")
-          .first()
-          .text()
-          .trim(),
-        Contents: Contents
-      });
+  for (let i = 0; i < $(".new").length; i++) {
+    $(".new")
+      .eq(i)
+      .each((index, element) => {
+        news.push({
+          ChannelName: $(element)
+            .find(".l")
+            .first()
+            .text()
+            .trim(),
+          Contents: []
+        });
 
-      // console.log(news[index].Contents);
-      $(element)
-        .find("li")
-        .each((ind, elet) => {
-          // for (let i = 0; i < news.length; i++) {
-          //   const item = news[i].Contents;
-          // }
-          // console.log(ind);
-          Contents.push({
-            Url:
-              "http://www.gl-ns.com" +
-              $(elet)
+        $(element)
+          .find("li")
+          .each((idx, emt) => {
+            news[i].Contents.push({
+              Url:
+                "http://www.gl-ns.com" +
+                $(emt)
+                  .find("a")
+                  .first()
+                  .attr("href"),
+              Title: $(emt)
                 .find("a")
                 .first()
-                .attr("href"),
-            Title: $(elet)
-              .find("a")
-              .first()
-              .text()
-              .trim(),
-            CreateTime: $(elet)
-              .find(".time")
-              .first()
-              .text()
-              .trim()
+                .text()
+                .trim(),
+              CreateTime: $(emt)
+                .find(".time")
+                .first()
+                .text()
+                .trim()
+            });
           });
-        });
-    });
-  // console.log(news);
+      });
+  }
+
+  console.log(news);
+
   fs.writeFile("./text.json", JSON.stringify(news), (err: object) => {
     if (err) {
       return console.error(err);
     }
-    console.log("文件写入成功");
     fs.readFile("./text.json", (err, data) => {
       if (err) {
         return console.error(err);
       }
-      // console.log(data.toString());
     });
   });
 };
 go();
-
-//todo
-class niumowang {
-  heard: string;
-  constructor(body: string) {
-    this.heard = body;
-  }
-  foot(wtf: number) {
-    console.log(`这个人有${wtf}条腿`);
-  }
-}
-let nmw = new niumowang("mmp");
-console.log(nmw.heard);
--- out/app.js
+++ out/app.js
@@ -11,74 +11,62 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const api = require("./api");
 const cheerio = require("cheerio");
 const fs = require("fs");
+class Channel {
+}
+exports.Channel = Channel;
+class Content {
+}
+exports.Content = Content;
 const go = () => __awaiter(this, void 0, void 0, function* () {
     const res = yield api.remote_get("http://www.gl-ns.com/");
     //加载HTML
     const $ = cheerio.load(res.text);
-    let Contents = [];
     let news = [];
-    let urls = [];
-    let titles = [];
-    let data = [];
-    $(".new").eq(0).each((index, element) => {
-        news.push({
-            ChannelName: $(element)
-                .find(".l")
-                .first()
-                .text()
-                .trim(),
-            Contents: Contents
-        });
-        // console.log(news[index].Contents);
-        $(element)
-            .find("li")
-            .each((ind, elet) => {
-            // for (let i = 0; i < news.length; i++) {
-            //   const item = news[i].Contents;
-            // }
-            // console.log(ind);
-            Contents.push({
-                Url: "http://www.gl-ns.com" +
-                    $(elet)
-                        .find("a")
-                        .first()
-                        .attr("href"),
-                Title: $(elet)
-                    .find("a")
+    for (let i = 0; i < $(".new").length; i++) {
+        $(".new")
+            .eq(i)
+            .each((index, element) => {
+            news.push({
+                ChannelName: $(element)
+                    .find(".l")
                     .first()
                     .text()
                     .trim(),
-                CreateTime: $(elet)
-                    .find(".time")
-                    .first()
-                    .text()
-                    .trim()
+                Contents: []
+            });
+            $(element)
+                .find("li")
+                .each((idx, emt) => {
+                news[i].Contents.push({
+                    Url: "http://www.gl-ns.com" +
+                        $(emt)
+                            .find("a")
+                            .first()
+                            .attr("href"),
+                    Title: $(emt)
+                        .find("a")
+                        .first()
+                        .text()
+                        .trim(),
+                    CreateTime: $(emt)
+                        .find(".time")
+                        .first()
+                        .text()
+                        .trim()
+                });
             });
         });
-    });
-    // console.log(news);
+    }
+    console.log(news);
     fs.writeFile("./text.json", JSON.stringify(news), (err) => {
         if (err) {
             return console.error(err);
         }
-        console.log("文件写入成功");
         fs.readFile("./text.json", (err, data) => {
             if (err) {
                 return console.error(err);
             }
-            // console.log(data.toString());
         });
     });
 });
 go();
-//todo
-class niumowang {
-    constructor(body) {
-        this.heard = body;
-    }
-    foot(wtf) {
-        console.log(`这个人有${wtf}条腿`);
-    }
-}
-let nmw = new niumowang("mmp");
-console.log(nmw.heard);
